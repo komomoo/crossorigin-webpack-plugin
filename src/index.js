4a3fbef744eb880f29909ad7aa297ca63fe89d50
momoko <ko.momo@qq.com>
2019-03-13 20:12:10 +0800

    feat: core

---
+++ src/index.js
@@ -0,0 +1,38 @@
+/**
+ * crossorigin-webpack-plugin
+ */
+
+module.exports = class CrossoriginWebpackPlugin {
+  constructor (options) {
+    this.options = options || { crossorigin: 'anonymous' }
+  }
+
+  apply (compiler) {
+    const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+    // Hook into the html-webpack-plugin processing
+    if (HtmlWebpackPlugin && HtmlWebpackPlugin.getHooks) {
+      // HtmlWebpackPlugin 4
+      HtmlWebpackPlugin.getHooks(compiler).alterAssetTags.tapAsync('CrossoriginWebpackPlugin', this._addAttributeToScripts.bind(this))
+    } else if (compiler.hooks) {
+      // HtmlWebpackPlugin 3
+      compiler.hooks.compilation.tap('CrossoriginWebpackPlugin', compilation => {
+        compilation.hooks.htmlWebpackPluginAlterAssetTags.tapAsync('CrossoriginWebpackPlugin', this._addAttributeToScripts.bind(this))
+      })
+    } else {
+      // HtmlWebpackPlugin 2
+      compiler.plugin('compilation', compilation => {
+        compilation.plugin(
+          'html-webpack-plugin-alter-asset-tags',
+          this._addAttributeToScripts.bind(this)
+        )
+      })
+    }
+  }
+
+  _addAttributeToScripts (htmlPluginData, callback) {
+    htmlPluginData.head.filter(tag => tag.tagName === 'script').forEach(script => (script.attributes.crossorigin = this.options.crossorigin))
+    htmlPluginData.body.filter(tag => tag.tagName === 'script').forEach(script => (script.attributes.crossorigin = this.options.crossorigin))
+    callback(null, htmlPluginData)
+  }
+}
